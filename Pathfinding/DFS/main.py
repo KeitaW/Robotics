cc784c2195044134b8da708a7d5465ab2f588fbf
Keita Watanabe <keitaw09@gmail.com>
2019-10-17 14:13:32 +0900

    Update

--- Pathfinding/DFS/main.py
+++ Pathfinding/DFS/main.py
@@ -1,13 +1,14 @@
 from Pathfinding.Maze.maze import Maze
 
 
-class DFS(Maze):
+class DFS():
     def __init__(self):
+        self.maze = Maze()
         self.state = "S"
         self.open_list = [self.state]
         self.close_list = []
 
-    def dfs(self):
+    def search(self):
         while self.open_list is not None:
             print("Open list", self.open_list)
             print("Close list", self.close_list)
@@ -15,3 +16,14 @@ class DFS(Maze):
             if state == "G":
                 return
             self.close_list.append(state)
+            self.open_list += [state for state in self.maze.get_actions(
+                state) if state not in self.close_list]
+
+
+if __name__ == "__main__":
+    print("Started")
+    dfs = DFS()
+    print("Graph", dfs.maze.G)
+    dfs.search()
+    print("Graph", dfs.maze.G)
+    dfs.maze.draw()
