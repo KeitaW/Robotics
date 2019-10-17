cd8c39b6fc59f9da676f5a68525b9ecfe2412b88
Keita Watanabe <keitaw09@gmail.com>
2019-10-17 15:24:00 +0900

    Added for pip install -e .

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+# with open("README.md", "r") as f:
+#    long_description = f.read()
+
+setup(
+    name="Personal notes on Robotics",
+    version="develop",
+    description="Just a log",
+    author="Keita Watanabe",
+    maintainer_email="keitaw09@gmail.com",
+    packages=find_packages(),
+)
