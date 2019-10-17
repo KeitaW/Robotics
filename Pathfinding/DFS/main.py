5489baca8a0334f8ccc1c71f79ceaa8381f86ac5
Keita Watanabe <keitaw09@gmail.com>
2019-10-17 14:21:21 +0900

    DFS implemented

--- Pathfinding/DFS/main.py
+++ Pathfinding/DFS/main.py
@@ -13,11 +13,15 @@ class DFS():
             print("Open list", self.open_list)
             print("Close list", self.close_list)
             state = self.open_list.pop(0)
+            print(f"Current State {state}")
             if state == "G":
+                print("Reaches goal!")
                 return
             self.close_list.append(state)
-            self.open_list += [state for state in self.maze.get_actions(
-                state) if state not in self.close_list]
+            self.open_list = [
+                state for state in self.maze.get_actions(
+                    state) if (state not in self.close_list) and (state not in self.open_list)
+            ] + self.open_list
 
 
 if __name__ == "__main__":
